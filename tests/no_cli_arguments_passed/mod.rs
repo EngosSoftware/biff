e9b6353c2208a94cc97048a9f3a10cade24b328a
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-05-27 17:34:00 +0200

    Updates.

--- tests/no_cli_arguments_passed/mod.rs
+++ tests/no_cli_arguments_passed/mod.rs
@@ -1,6 +1,7 @@
 use super::*;
 use assert_cmd::Command;
 
+#[cfg(target_os = "linux")]
 #[test]
 fn _0001() {
   let mut cmd = Command::cargo_bin("biff").unwrap();
@@ -15,6 +16,27 @@ fn _0001() {
   <FILE2>
 
 Usage: biff <FILE1> <FILE2>
+
+For more information, try '--help'.
+"#,
+    );
+}
+
+#[cfg(target_os = "windows")]
+#[test]
+fn _0002() {
+  let mut cmd = Command::cargo_bin("biff").unwrap();
+  cmd
+    .current_dir(current_dir(file!()))
+    .assert()
+    .code(2)
+    .stdout("")
+    .stderr(
+      r#"error: the following required arguments were not provided:
+  <FILE1>
+  <FILE2>
+
+Usage: biff.exe <FILE1> <FILE2>
 
 For more information, try '--help'.
 "#,
--- tests/read_error/mod.rs
+++ tests/read_error/mod.rs
@@ -29,6 +29,7 @@ fn _0002() {
     .stderr("Unexpected: Os { code: 2, kind: NotFound, message: \"The system cannot find the file specified.\" }\n");
 }
 
+#[cfg(target_os = "linux")]
 #[test]
 fn _0003() {
   let mut cmd = Command::cargo_bin("biff").unwrap();
@@ -41,9 +42,22 @@ fn _0003() {
     .stderr("Unexpected: Os { code: 2, kind: NotFound, message: \"No such file or directory\" }\n");
 }
 
-#[cfg(target_os = "linux")]
+#[cfg(target_os = "windows")]
 #[test]
 fn _0004() {
+  let mut cmd = Command::cargo_bin("biff").unwrap();
+  cmd
+    .current_dir(current_dir(file!()))
+    .arg("file.txt")
+    .arg("a.txt")
+    .assert()
+    .code(2)
+    .stderr("Unexpected: Os { code: 2, kind: NotFound, message: \"The system cannot find the file specified.\" }\n");
+}
+
+#[cfg(target_os = "linux")]
+#[test]
+fn _0005() {
   let mut cmd = Command::cargo_bin("biff").unwrap();
   cmd
     .current_dir(current_dir(file!()))
@@ -59,7 +73,7 @@ fn _0004() {
 
 #[cfg(target_os = "linux")]
 #[test]
-fn _0005() {
+fn _0006() {
   let mut cmd = Command::cargo_bin("biff").unwrap();
   cmd
     .current_dir(current_dir(file!()))
