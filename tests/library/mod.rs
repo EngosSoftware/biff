ed639f9d107d162c31ea43aee7206809b87c64a0
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-05-29 14:55:48 +0200

    Updates.

--- tests/library/mod.rs
+++ tests/library/mod.rs
@@ -43,3 +43,27 @@ fn _0004() {
     ComparisonResult::SimilarPercentage(50.0, 25.0)
   );
 }
+
+#[test]
+fn _0005() {
+  let input_1: &[u8] = &[0, 1, 2, 3];
+  let input_2: &[u8] = &[0, 1, 3, 4];
+  let mut options = ComparisonOptions::default();
+  options.absolute_limit = Some(1);
+  assert_eq!(
+    compare(input_1, input_2, &options),
+    ComparisonResult::AbsoluteLimitExceeded(1, 2)
+  );
+}
+
+#[test]
+fn _0006() {
+  let input_1: &[u8] = &[0, 1, 2, 3];
+  let input_2: &[u8] = &[0, 1, 3, 4];
+  let mut options = ComparisonOptions::default();
+  options.percentage_limit = Some(25.0);
+  assert_eq!(
+    compare(input_1, input_2, &options),
+    ComparisonResult::PercentageLimitExceeded(25.0, 50.0)
+  );
+}
